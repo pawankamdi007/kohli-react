2b119d53dbb63fbc6f114d59a953cccc2d60d0aa
pawan <pawankamdi02@gmail.com>
2020-11-04 15:54:16 +0530

    added change color, change font and font style download btn

--- src/comp/MemeApp.jsx
+++ src/comp/MemeApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 const MemeApp=()=>{
     
     const[upper,setUpper]=useState();
@@ -7,6 +8,10 @@ const MemeApp=()=>{
     const[bottom,setBottom]=useState();
     const[newBottom,setNewBottom]=useState();
     const[image,setImage]=useState("https://picsum.photos/300/300"); 
+    const [fontSize,setFontSize]=useState(35);
+    const[fontFamily,setFontFamily]=useState("helvetica");
+    const[fontColor,setFontColor]=useState("white");
+
     const inputEvent=(event)=>{
         setUpper(event.target.value);   
     }
@@ -25,6 +30,20 @@ const MemeApp=()=>{
     setImage( URL.createObjectURL(img));
     
     }
+    const font=(event)=>{
+      console.log(event.target.value);
+      setFontSize(event.target.value);
+        //$("#upper").css("font-size","fontSize");
+    } 
+    const fontFamilyFun=(event)=>{
+        console.log(event.target.value);
+        setFontFamily(event.target.value);
+    }
+    const fontColorFun=(event)=>{
+        console.log(event.target.value);
+        setFontColor(event.target.value);
+    }
+    
     const img="https://picsum.photos/300/400";
     return(
         <>
@@ -33,23 +52,36 @@ const MemeApp=()=>{
         </div>
            <div className="bg-success"> 
 
-            <div className="container bg-dark">
+            <div className="container bg-dark" style={{height:"100vh"}}>
             <div className="row"> 
             <div className="col-12  meme p-3">
-            <img id="main-img" className="img-thumbnail d-block mx-auto " width="300px" height="300px" src={image} alt=""/>     
-            <h1 className="top"> {newUpper}</h1>
-            <h1 className="bottom">{newBottom}</h1>
+            <a href={image} download="w3logo.jpg">
+            <img id="main-img" className="img-thumbnail d-block mx-auto " width="300px" height="300px" src={image} alt=""  />     
+            </a>
+            <p id="upper" className="top" style={{fontSize:`${fontSize}px`,fontFamily:fontFamily,color:fontColor}}> {newUpper}</p>
+            <h1 id="bottom" className="bottom" style={{fontSize:`${fontSize}px`,fontFamily:fontFamily,color:fontColor}} >{newBottom}</h1>
             </div>
             </div>
            
             <br />
-            <div className="container cont p-3">
-            <input className="form-control col-sm-3 mx-auto" type="text" onChange={inputEvent} value={upper} placeholder="Upper text"/><br />
-            <input className="form-control col-sm-3 mx-auto" type="text" onChange={inputEvent2} placeholder="Bottom text"/><br />
-            <input className="form-control col-sm-3 mx-auto" type="file" name="myImage" onChange={imageUpload} /><br/>
-            <input className="btn btn-success col-sm-3 mx-auto  mt-2 mb-5 submit" onClick={fun} value="submit" type="submit"/>
-            </div>
+            <div className="container cont p-3 form-inline">
+            <input className="form-control col-sm-4 mx-auto mb-2" type="text" onChange={inputEvent} value={upper} placeholder="Upper text"/><br />
+            <input className="form-control col-sm-4 mx-auto mb-2" type="text" onChange={inputEvent2} placeholder="Bottom text"/><br />
+            <input className="form-control col-sm-3 mx-auto mb-2" type="file" name="myImage" onChange={imageUpload} /><br/>
+            <input className="form-control  col-sm-4 mx-auto mb-2" type="number" onChange={font} min="10" max="35"  placeholder="Font Size"/>
+            <input className="form-control  col-sm-4 mx-auto mb-2" type="text" onChange={fontColorFun}   placeholder="Font Color"/>
 
+            <select className="form-control  col-sm-3 mx-auto  mb-2" onChange={fontFamilyFun} type="select">
+            <option defaultValue>Select Font style</option>
+            <option value="Comic Sans MS">Comic Sans MS</option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="Courier New">Courier New</option>
+            </select>
+            </div>
+            <div className=" container form-inline">
+            <input className="btn btn-success col-sm-3 mx-auto  mt-3 mb-3 submit" onClick={fun} value="submit" type="submit"/>
+            <input className="btn btn-success col-sm-3 mx-auto  mt-3 mb-5 " value="Download" type="button"/>
+            </div>
             </div>
             </div>
             </>
